af320550aaea3b73c48052cec415205adff0a047
Joshua Olson <joshua.olson@gmail.com>
2011-12-07 00:11:40 -0800

    Added column-79 marker and white space highlighing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,23 +1,20 @@
+;; Joshua Olson init.el
 ;; Process scripts and external libraries
+
 ;; Add all top-level subdirectories of .emacs.d to the load path
 (add-to-list 'load-path "~/.emacs.d/")
-(progn (cd "~/.emacs.d")
-       (normal-top-level-add-subdirs-to-load-path))
-
 ;; Third party libraries are stored in ~/.emacs.d/extern
 (add-to-list 'load-path "~/.emacs.d/extern")
-(progn (cd "~/.emacs.d/extern")
-       (normal-top-level-add-subdirs-to-load-path))
-
-;; Requires
-(require 'tramp)
-(require 'xscheme)
-;; External requires
-(require 'color-theme-zenburn)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\|[tT]\\)\\'" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
 
 ;; Set variables
-(setq inhibit-splash-screen t)
+(setq-default show-trailing-whitespace t)
 (setq-default cperl-indent-level 4)
+(setq inhibit-splash-screen t)
 (setq debug-on-error t)
 (setq make-backup-files nil)
 (setq initial-frame-alist '((top . 10) (left . 30)
@@ -27,19 +24,35 @@
 (setq scheme-program-name
       "~/Local Applications/Development/mit-scheme.app/Contents/Resources/mit-scheme")
 
-(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\|[tT]\\)\\'" . cperl-mode))
-(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
-(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
-(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
+;; Autoload
+(autoload 'js2-mode "js2" nil t)
+
 ;; Setting to run right away
+(progn (cd "~/.emacs.d/extern")
+       (normal-top-level-add-subdirs-to-load-path))
+(progn (cd "~/.emacs.d")
+       (normal-top-level-add-subdirs-to-load-path))
 (progn (split-window-horizontally)
        (desktop-save-mode 1)
        )
+
+;; Requires
+(require 'tramp)
+(require 'xscheme)
+
+;; External requires
+(require 'color-theme-zenburn)
+(require 'column-marker)
+
+;; Hooks
+(add-hook 'font-lock-mode-hook (lambda () (interactive) (column-marker-3 79)))
+(add-hook 'mouse-leave-buffer-hook 'stop-using-minibuffer)
 
 ;; Window system specific
 (if window-system
     (progn (color-theme-zenburn)
 	   ))
+
 ;; Set the initial working directory
 (progn (cd "~/"))
 
@@ -57,3 +70,9 @@
   ;; If there is more than one, they won't work right.
 ; (set-face-attribute 'default nil :height 180)
  )
+
+;; Function for fixing mouse use mini-buffer errors
+(defun stop-using-minibuffer ()
+  "kill the minibuffer"
+  (when (and (>= (recursion-depth) 1) (active-minibuffer-window))
+    (abort-recursive-edit)))
