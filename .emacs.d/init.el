e1a027ce7a0388dd82747ab5349b1f6388a73a48
Joshua Olson <joshua.olson@gmail.com>
2011-12-21 08:47:37 -0800

    Updated init.el with global font lock mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,7 +75,8 @@ Contents/Resources/mit-scheme")
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(column-number-mode t))
+ '(column-number-mode t)
+ '(global-font-lock-mode t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
