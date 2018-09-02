6c10a59926841a5e8cebacea8d6c44ca35e75c0f
Joshua Olson <jolson@linkedin.com>
2018-09-01 20:01:25 -0700

    Move install code higher so it won't conflict with linking

--- setup_env.sh
+++ setup_env.sh
@@ -8,15 +8,7 @@ mkdir -p ${HOME}/bin
 # Requires git and ssh key to be setup
 ! [ -e ${HOME}/bin/zsh-git-prompt ] && git clone https://github.com/olivierverdier/zsh-git-prompt.git ${HOME}/bin/zsh-git-prompt
 
-find $pwd -name ".DS_Store" -delete
-
-# Link all the packages
-excluded="ssh vim"
-link_packages `find_stow_packages $excluded`
-# Special setups
-echo "Linking: ssh"
-stow --target=${HOME}/.ssh/ ssh
-
+# Install stuff before depending on it with later commands
 # OS Specific setups
 case "$(uname -s)" in
     Darwin) # Install macOS stuff
@@ -28,3 +20,12 @@ case "$(uname -s)" in
 	${pwd}/setup_linux_env.sh
 	;;
 esac
+
+find $pwd -name ".DS_Store" -delete
+
+# Link all the packages
+excluded="ssh vim"
+link_packages `find_stow_packages $excluded`
+# Special setups
+echo "Linking: ssh"
+stow --target=${HOME}/.ssh/ ssh
